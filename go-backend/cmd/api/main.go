4a1a69168a94ccbbc9c3836435f9a9cd1ad243ed
Soontorn Choosenpom <soontornc008@gmail.com>
2025-08-01 12:39:08 +0700

    edit swagger URL

--- go-backend/cmd/api/main.go
+++ go-backend/cmd/api/main.go
@@ -10,7 +10,7 @@
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:4001
+// @host 192.168.1.8:4001
 // @BasePath /api/v1
 // @securityDefinitions.apikey BearerAuth
 // @in header
